bfbb73c51102c684d3927e16a2a2402af82390c3
guicdas <silva.caldeira.guilherme@gmail.com>
2026-04-09 00:00:08 +0100

    weird npm bugs

--- svelte.config.js
+++ svelte.config.js
@@ -1,2 +1,9 @@
+// svelte.config.js
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default {};
+const config = {
+	preprocess: vitePreprocess({ script: true }),
+};
+
+export default config;
